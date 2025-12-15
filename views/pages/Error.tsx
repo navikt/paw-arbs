52d0245e9851d45a76000b131f9c443d85447e77
Christian Rennemo <christian.rennemo@nav.no>
2025-12-15 14:45:31 +0100

    Legg til stil for feilmeldingsside og implementer Error-komponent

---
+++ views/pages/Error.tsx
@@ -0,0 +1,16 @@
+import { FC } from "hono/jsx";
+import BaseLayout from "../layouts/BaseLayout.tsx";
+import Header from "../layouts/Header.tsx";
+
+const Error: FC<{ message: string }> = ({ message }) => {
+  return (
+    <BaseLayout title="Feilmelding">
+      <Header />
+      <div class="error-page">
+        <h1>Feil oppstod</h1>
+        <p>{message}</p>
+      </div>
+    </BaseLayout>
+  );
+}
+export { Error };
