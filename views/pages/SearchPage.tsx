d06fa59dac7bca466e94345fbd76a39cc338a083
Christian Rennemo <christian.rennemo@nav.no>
2026-01-29 10:35:25 +0100

    ta inn snapshot

--- views/pages/SearchPage.tsx
+++ views/pages/SearchPage.tsx
@@ -3,10 +3,19 @@ import BaseLayout from "@/views/layouts/BaseLayout.tsx";
 import Header from "@/views/layouts/Header.tsx";
 import { ArbeidsoekerCard } from "@/views/components/ArbeidsoekerCard.tsx";
 import type { ArbeidsoekerDetaljer } from "@/types.ts";
+import { Snapshot } from "@navikt/arbeidssokerregisteret-utils/oppslag/v3";
+import { Opplysninger } from "../components/Opplysninger.tsx";
+
+type SearchPageProps = {
+  title: string;
+  searchQuery?: string;
+  detaljer?: ArbeidsoekerDetaljer;
+  snapshot?: Snapshot | null;
+};
 
 const SearchPage: FC<
-  { title: string; searchQuery?: string; detaljer?: ArbeidsoekerDetaljer }
-> = ({ title, searchQuery, detaljer }) => {
+  SearchPageProps
+> = ({ title, searchQuery, detaljer, snapshot }) => {
   return (
     <BaseLayout title={title}>
       <Header />
@@ -15,10 +24,14 @@ const SearchPage: FC<
           ? <h2>Søkeresultater for "{searchQuery}":</h2>
           : <h2>Ingen søkeparametere er oppgitt</h2>}
 
-        <div>
-          {detaljer
-            ? <ArbeidsoekerCard detaljer={detaljer} />
-            : <p>Ingen data funnet</p>}
+        <div class="card-grid">
+          <div>
+            {detaljer
+              ? <ArbeidsoekerCard detaljer={detaljer} />
+              : <p>Ingen data funnet</p>}
+          </div>
+
+          {snapshot && <Opplysninger data={snapshot} />}
         </div>
       </main>
     </BaseLayout>
